23eef717a150c6c15618d60acc3854d5effbf76d
Кирилл Б <ferdinandhinick@gmail.com>
2025-01-13 19:52:00 +0300

    Паралакс

--- danik game/game.cpp
+++ danik game/game.cpp
@@ -12,27 +12,50 @@ using namespace sf;
 /////// Зона которая реагирует на вхождение в нее игрока 
 struct TriggerZone {
     sf::RectangleShape shape;
+    sf::Text text;
+    bool isPlayerInside = false;
 
-    TriggerZone(sf::Vector2f position, sf::Vector2f size) {
+    TriggerZone(sf::Vector2f position, sf::Vector2f size, const sf::Font& font, const std::string& message) {
+        // Инициализация формы зоны
         shape.setPosition(position);
         shape.setSize(size);
         shape.setFillColor(sf::Color(255, 0, 0, 100)); // Полупрозрачный красный
+
+        // Инициализация текста
+        text.setFont(font);
+        text.setString(message);
+        text.setCharacterSize(24);
+        text.setFillColor(sf::Color::White); // Белый цвет текста
+        text.setOutlineColor(sf::Color::Black); // Чёрная обводка
+        text.setOutlineThickness(2);
+        text.setPosition(position.x, position.y - 30); // Располагаем текст над зоной
     }
 
     // Метод проверки вхождения спрайта в зону
-    bool checkCollision(const sf::Sprite& sprite) const {
-        return shape.getGlobalBounds().intersects(sprite.getGlobalBounds());
+    void checkCollision(const sf::Sprite& sprite) {
+        isPlayerInside = shape.getGlobalBounds().intersects(sprite.getGlobalBounds());
+    }
+
+
+    // Метод отрисовки зоны и текста
+    void draw(sf::RenderWindow& window) {
+        window.draw(shape);
+        if (isPlayerInside) {
+            window.draw(text);
+        }
     }
 };
 /////// Зона которая реагирует на вхождение в нее игрока 
 
 /////PARALAX
 
-sf::Vector2f getAspectRatio(const sf::Texture& texture,float& aspectRatio) {
+/////Функция для подсчета количества кадров в текстуре
+sf::Vector2f getAspectRatio(const sf::Texture& texture,float& aspectRatio){
     sf::Vector2u size = texture.getSize();
     aspectRatio = static_cast<float>(size.y) / size.x;
     return sf::Vector2f(aspectRatio, 1.0f);
 }
+/////Булевая Функция для проверки коллизиии
 bool checkSpriteOverlap(sf::Sprite playerSprite, std::vector<sf::Sprite> otherSprites) {
     sf::FloatRect playerBounds = playerSprite.getGlobalBounds();
 
@@ -46,6 +69,7 @@ bool checkSpriteOverlap(sf::Sprite playerSprite, std::vector<sf::Sprite> otherSp
     
     return false; 
 }
+/////Булевая Функция для проверки коллизиии
 bool checkSprite(sf::Sprite playerSprite, sf::Sprite otherSprite) {
     sf::FloatRect playerBounds = playerSprite.getGlobalBounds();
     sf::FloatRect otherBounds = otherSprite.getGlobalBounds();
@@ -68,6 +92,7 @@ int main()
      
      ///PARALAX
     int colparalax = 6;
+    bool moveParalax =  true;
      
     std::vector<sf::Texture> paralaxTexture(colparalax);
     
@@ -144,6 +169,8 @@ for(int i = 0; i < 20; i++){
 
 ///Создание переменных для логики игрока  
 
+sf::Vector2f SpawnPlayerPosition;//переменная для запоминания спавна игрока
+
 int j=0,anim=6,upper=0;
 float frame;
 bool moveR = false;
@@ -162,75 +189,6 @@ rectangleFall.setColor(Color(0,255,0));
 Sprite rectangleX;
 rectangleX.setColor(Color(0,0,255));
 
-///Создание переменных для логики игрока  
-	
-///-----
-
-Image map_image;
-
-	map_image.loadFromFile("animation/tile.png");
-	Texture obstacleTexture;
-	obstacleTexture.loadFromImage(map_image);	
-	rectangley.setTexture(obstacleTexture);
-	rectangley.setTextureRect(IntRect(0, 100, 20, 20));
-	rectangley2.setTexture(obstacleTexture);
-	rectangley2.setTextureRect(IntRect(0, 100, 20, 80));	
-	rectangleX.setTexture(obstacleTexture);
-	rectangleX.setTextureRect(IntRect(0, 100,50, 50));
-	rectangleFall.setTexture(obstacleTexture);
-	rectangleFall.setTextureRect(IntRect(0, 100, 100, 100));
-
-	std::vector<TriggerZone> triggerZones; // Хранилище для триггерных зон
-    std::vector<sf::Sprite> obstacleSprites;
-    
- 		for (int i = 0; i < HEIGHT_MAP; i++)
-		for (int j = 0; j < WIDTH_MAP; j++){if((TileMap[i][j] == '0')){
-		        sf::Sprite obstacleSprite(obstacleTexture);
-        obstacleSprite.setTextureRect(IntRect(0, 100, 100, 35));
-        obstacleSprite.setPosition(-100 + j * 100, -300 + i * 145);
-        obstacleSprites.push_back(obstacleSprite);}
-        if((TileMap[i][j] == 's')){
-		        sf::Sprite obstacleSprite(obstacleTexture);
-        obstacleSprite.setTextureRect(IntRect(0, 100, 100, 145));
-        obstacleSprite.setPosition(-100 + j * 100, -300 + i * 145);
-        obstacleSprites.push_back(obstacleSprite);
-		}
-        
-                if((TileMap[i][j] == 'g')){
-		        sf::Sprite obstacleSprite(obstacleTexture);
-        obstacleSprite.setTextureRect(IntRect(0, 100, 100, 35));
-        obstacleSprite.setPosition(-100 + j * 100, -300 + (i * 145) - 10);
-        obstacleSprites.push_back(obstacleSprite);
-		}
-                    if (TileMap[i][j] == 'i') { // Создание триггерной зоны
-                sf::Vector2f position(-300 + j * 100, -300 + i * 145);
-                sf::Vector2f size(300, 145); // Задайте размер триггерной зоны
-                triggerZones.emplace_back(position, size);
-            }
-  
-
-    }
-std::vector<sf::Sprite> sprite1Coin2;
-for (int i = 0; i < HEIGHT_MAP; i++) {
-    for (int j = 0; j < WIDTH_MAP; j++) {
-        if (TileMap[i][j] == 'm') {
-            sf::Sprite sprite1(textureCoin);
-            sprite1.setTextureRect(sf::IntRect((frameCoinWidht*coinH),(0),frameCoinWidht,frameCoinHeight));
-            sprite1.setScale(0.2,0.2);
-            sprite1.setPosition(-100 + j * 100, -200 + i * 145);
-            sprite1Coin2.push_back(sprite1);
-        }
-    }
-}
-//Создание страйта монетки
-Sprite spriteCoin;
-spriteCoin.setScale(0.15,0.15);
-spriteCoin.setTexture(textureCoin);
-spriteCoin.setPosition(1,-50);
-spriteCoin.setTextureRect(sf::IntRect((frameCoinWidht*coinH),(0),frameCoinWidht,frameCoinHeight));
-//Создание страйта монетки
-
-//----------------
 
 //Массив на именами анимаций
     string file[anim];
@@ -254,7 +212,7 @@ spriteCoin.setTextureRect(sf::IntRect((frameCoinWidht*coinH),(0),frameCoinWidht,
 }
 //Загрузка текстур с анимациями персонажа
 
-//----------------
+
 
 //Создание спрайта игрока 
     Sprite sprite;
@@ -263,8 +221,100 @@ spriteCoin.setTextureRect(sf::IntRect((frameCoinWidht*coinH),(0),frameCoinWidht,
     sprite.setScale(2.0f,2.0f);
 //Создание спрайта игрока
 
-//----------------
  
+ 
+///Создание переменных для логики игрока  
+	
+///-----
+
+Image map_image;
+
+	map_image.loadFromFile("animation/tile.png");///Загрузка тайлсета карты
+	Texture obstacleTexture;///создание текстуры тайлсета карты
+	obstacleTexture.loadFromImage(map_image);///загрузка тайлсета карты	в спрайты
+	
+////------	
+
+//// Области для проверки коллзии игрока
+	rectangley.setTexture(obstacleTexture);
+	rectangley.setTextureRect(IntRect(0, 100, 20, 20));
+	rectangley2.setTexture(obstacleTexture);
+	rectangley2.setTextureRect(IntRect(0, 100, 20, 80));	
+	rectangleX.setTexture(obstacleTexture);
+	rectangleX.setTextureRect(IntRect(0, 100,50, 50));
+	rectangleFall.setTexture(obstacleTexture);
+	rectangleFall.setTextureRect(IntRect(0, 100, 100, 100));
+//// Области для проверки коллзии игрока
+
+////------
+	
+////------	
+
+	std::vector<TriggerZone> triggerZones; // Хранилище для триггерных зон
+    std::vector<sf::Sprite> obstacleSprites;// Хранилище для тайлов карты
+    
+////------ 
+
+///Растановка тайлов карты из текстового массива  
+    
+ 		for (int i = 0; i < HEIGHT_MAP; i++)
+		for (int j = 0; j < WIDTH_MAP; j++){if((TileMap[i][j] == '0')){
+		        sf::Sprite obstacleSprite(obstacleTexture);
+        obstacleSprite.setTextureRect(IntRect(0, 100, 100, 35));
+        obstacleSprite.setPosition(-100 + j * 100, -300 + i * 145);
+        obstacleSprites.push_back(obstacleSprite);}
+        if((TileMap[i][j] == 'p')){
+        sprite.setPosition(-100 + j * 100, -300 + i * 145);
+        SpawnPlayerPosition = Vector2f(-100 + j * 100, -300 + i * 145);
+		}
+		if((TileMap[i][j] == 's')){
+		        sf::Sprite obstacleSprite(obstacleTexture);
+        obstacleSprite.setTextureRect(IntRect(0, 100, 100, 145));
+        obstacleSprite.setPosition(-100 + j * 100, -300 + i * 145);
+        obstacleSprites.push_back(obstacleSprite);
+		}
+        
+                if((TileMap[i][j] == 'g')){
+		        sf::Sprite obstacleSprite(obstacleTexture);
+        obstacleSprite.setTextureRect(IntRect(0, 100, 100, 35));
+        obstacleSprite.setPosition(-100 + j * 100, -300 + (i * 145) - 10);
+        obstacleSprites.push_back(obstacleSprite);
+		}
+                    if (TileMap[i][j] == 'i') { // Создание триггерной зоны
+                sf::Vector2f position(-300 + j * 100, -300 + i * 145);
+                sf::Vector2f size(300, 145); // Задайте размер триггерной зоны
+                triggerZones.emplace_back(position, size,font, "Нажмите E");
+            }
+  
+
+    }
+///Растановка тайлов карты из текстового массива  
+    
+////------
+
+std::vector<sf::Sprite> sprite1Coin2;// Хранилище для спрайтов монет
+
+for (int i = 0; i < HEIGHT_MAP; i++) {
+    for (int j = 0; j < WIDTH_MAP; j++) {
+        if (TileMap[i][j] == 'm') {
+            sf::Sprite sprite1(textureCoin);
+            sprite1.setTextureRect(sf::IntRect((frameCoinWidht*coinH),(0),frameCoinWidht,frameCoinHeight));
+            sprite1.setScale(0.2,0.2);
+            sprite1.setPosition(-100 + j * 100, -200 + i * 145);
+            sprite1Coin2.push_back(sprite1);
+        }
+    }
+}
+//Создание страйта монетки
+Sprite spriteCoin;
+spriteCoin.setScale(0.15,0.15);
+spriteCoin.setTexture(textureCoin);
+spriteCoin.setPosition(1,-50);
+spriteCoin.setTextureRect(sf::IntRect((frameCoinWidht*coinH),(0),frameCoinWidht,frameCoinHeight));
+//Создание страйта монетки
+
+//----------------
+
 
     getAspectRatio(texture[sheet],frame);
     int time1 = 0,time2 = 0;
@@ -375,24 +425,33 @@ if ((Keyboard::isKeyPressed(Keyboard::Right))){
        			rectangley2.setPosition(sprite.getPosition().x+40,sprite.getPosition().y-11);
        			rectangleX.setPosition(sprite.getPosition().x+25,sprite.getPosition().y+10);
        		rectangleFall.setPosition(sprite.getPosition().x,sprite.getPosition().y+28);
-       	        if (!checkSpriteOverlap(rectangley, obstacleSprites)){
+       		
+ /// -------  
+ 
+ 
+ /// ВСЕ ЧТО ЗВЯЗАНО С КОЛЛИЗИЕЙ ////		
+
+    		
+       		
+    if (!checkSpriteOverlap(rectangley, obstacleSprites)){
     speeddown += 0.4;
 	sprite.move(0,speeddown+7);
         }else{
-speeddown = 0;
-        		        	if(upR && frame <= j && up or upR && checkSpriteOverlap(rectangley, obstacleSprites ) && up ){
-up = false;
-j =0 ;
-sheet = 0;
-getAspectRatio(texture[sheet],frame);
-sprite.setTexture(texture[sheet]);	
-			}
+        	
+			speeddown = 0;
+        	if(upR && frame <= j && up or upR && checkSpriteOverlap(rectangley, obstacleSprites ) && up ){
+			up = false;
+			j =0 ;
+			sheet = 0;
+			getAspectRatio(texture[sheet],frame);
+			sprite.setTexture(texture[sheet]);	
+						}
         	if(upL && frame <= j && up or upL && checkSpriteOverlap(rectangley, obstacleSprites) && up ){
-up = false ;        		
-j = 0 ;
-sheet = 1 ;
-getAspectRatio(texture[sheet],frame);
-sprite.setTexture(texture[sheet]);	
+				up = false ;        		
+				j = 0 ;
+				sheet = 1 ;
+				getAspectRatio(texture[sheet],frame);
+				sprite.setTexture(texture[sheet]);	
 			}
 
 		}
@@ -401,26 +460,38 @@ sprite.setTexture(texture[sheet]);
 //if(j==frame){
 //    	j=0;
 //	}	
-int totalFrames = texture[sheet].getSize().x / 50; // ????? ?????????? ??????
+int totalFrames = texture[sheet].getSize().x / 50; 
 j = (j + 1) % totalFrames;
+
 	 if (!checkSpriteOverlap(rectangleFall, obstacleSprites) && up && upper>(11) ) {
-     j  = 11;}
+     j  = 11;
+	 }
    
        	sprite.setTextureRect(sf::IntRect(50*j,0,50,50)); // Запуск анимации текстуры персонажа j меняет кадр 
        	
 	if (checkSpriteOverlap(rectangley2, obstacleSprites) ){
 	sprite.move(0,speedPlaer);
         }
-       if (checkSpriteOverlap(rectangleX, obstacleSprites)){
-if (moveR){
-sprite.move(-speedPlaer,0);	
-}
-if (moveL){
-sprite.move(speedPlaer,0);	
-}
+ 
+ 
+ ///   Проверка колизии по оси X    
+if (checkSpriteOverlap(rectangleX, obstacleSprites)){
+	moveParalax = false ; /// запрет на движение паралакса	
+	if (moveR){ ///Если персонаж движется в право , то его отталкивает в лево
+	sprite.move(-speedPlaer,0);	
+	}
+	if (moveL){///Если персонаж движется в лево , то его отталкивает в право
+	sprite.move(speedPlaer,0);	
+	}
+  }else{
+moveParalax = true ;/// разрешение на движение паралакса	
 } 
 		
-		
+/// ВСЕ ЧТО ЗВЯЗАНО С КОЛЛИЗИЕЙ ////		
+
+/// -------
+
+/// Плавное следдование камеры за игроком , с ускорением , чем дальше камера от игрока тем быстрее она движется
 		
 		if (x < sprite.getPosition().x){
 			x+=2;
@@ -454,6 +525,11 @@ sprite.move(speedPlaer,0);
 		if (y - 30 < sprite.getPosition().y){
 			y+=3;
 		}
+	/// Плавное следдование камеры за игроком , с ускорением , чем дальше камера от игрока тем быстрее она движется	
+		
+	/// -------
+	
+	
 	sf::Time elapsed = clock.getElapsedTime();
 if (elapsed >= delay)
         {
@@ -479,40 +555,42 @@ coinH = 0;
         
         ///Отрисовка и обработка паралакса
 
-// ?????????? ??? ???????? ??????????? ????????
-int moveDirection = 0;  // 0 - ??? ????????, -1 - ?????, 1 - ??????
+// Переменная для направления движения слоев паралакса
+int moveDirection = 0;  // 0 - на месте, -1 - лево, 1 - право
 
-// ? ????? ????????? ??????? ??????
+// Проверка направления движения игрока по клавишам
 if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-    moveDirection = -1;  // ??????????? ?????
+    moveDirection = -1;  // движение в лево
 } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-    moveDirection = 1;  // ??????????? ??????
+    moveDirection = 1;  // движение в право
 } else {
-    moveDirection = 0;  // ??? ????????
+    moveDirection = 0;  // Нет движения
 }
 
-// ??????? ????????? ? ??????????? ?? ???????????
+// Цикл для обработтки паралакса
 for (int i = 0; i < colparalax; i++) {
     float offset = 0.0f;
 
-    // ?????????? ???????? ??? ??????? ???? (??? ?????? ??????, ??? ??????? ????????)
-    float speed = (6 - i) / 6.5f;  // ??????????? ???????? ??? ??????? ????
+    float speed = (i - 2) / 1.5f;  // Скорость движения паралакса
 
-    // ???? ???????? ?????, ???????? ?? ????????????? ????????
+    // Проверка есть ли движение персонажа , и в какую сторону он движется
     if (moveDirection == -1) {
-        offset = speed;  // ???????? ?? ????????
+        offset = speed;  // Направление движения паралакса
     }
-    // ???? ???????? ??????, ???????? ?? ????????????? ????????
+    // Проверка есть ли движение персонажа , и в какую сторону он движется
     else if (moveDirection == 1) {
-        offset = -speed;  // ???????? ?? ????????
+        offset = -speed;  // Направление движения паралакса
     }
 
-    // ????????? ??????? ???? ????? ?????????? ? ??????????? ?? ??????????? ? ????????
+    // Перемещение паралакса
+    if(moveParalax){
     paralaxSprite[i].setPosition(paralaxSprite[i].getPosition().x + offset, -400 - (20 * (10 - i)));
     paralaxSprite1[i].setPosition(paralaxSprite1[i].getPosition().x + offset, -400 - (20 * (10 - i)));
-    paralaxSprite2[i].setPosition(paralaxSprite2[i].getPosition().x + offset, -400 - (20 * (10 - i)));
+    paralaxSprite2[i].setPosition(paralaxSprite2[i].getPosition().x + offset, -400 - (20 * (10 - i)));  	
+	}
 
-    // ?????? ???????
+
+    // Отрисовка паралакса
     window.draw(paralaxSprite[i]);
     window.draw(paralaxSprite1[i]);
     window.draw(paralaxSprite2[i]);
@@ -521,34 +599,20 @@ for (int i = 0; i < colparalax; i++) {
 	
         ///Отрисовка и обработка паралакса
         
-        
-        
-        
-        
-        
 
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
- 
         window.draw(sprite);
 
 ///Отрисовка карты  и ее элементов
         for (const sf::Sprite obstacle : obstacleSprites) {
             window.draw(obstacle);
         }
-		        for (const auto& trigger : triggerZones) {
+		    for (auto& trigger : triggerZones) {
+			   
+        trigger.checkCollision(sprite);// Проверяем зашел ли игрок в зону 
             window.draw(trigger.shape);
         }
+        
+        
 
 ///Отрисовка карты  и ее элементов
 		        for ( sf::Sprite sprite1 : sprite1Coin2) {
@@ -572,6 +636,13 @@ for (int i = 0; i < colparalax; i++) {
 	  window.draw(text);
       window.setView(view);
       
+      /////Доработка 
+      
+      if(sprite.getPosition().y > 1200){//Если игрок падает ниже этой границы , то его телепортирует на точку спавна
+      	sprite.setPosition(SpawnPlayerPosition);
+	  }
+      
+      ////Доработка 
 
         window.display();
     }
